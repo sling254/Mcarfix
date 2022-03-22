3a7a6c173a91227b4e0de093b60fb5df5d8ced80
sling254 <mwanikidenis7@gmail.com>
2022-03-22 12:23:26 +0300

    add PairMechanic,Mechanic and service model and migrations to the models

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ShopInventory,Mechanic,ServiceOffered,PairMechanic
 # Register your models here.
+
+
+admin.site.register(ShopInventory)
+admin.site.register(Mechanic)
+admin.site.register(ServiceOffered)
+admin.site.register(PairMechanic)
--- apis/models.py
+++ apis/models.py
@@ -8,11 +8,24 @@ class ShopInventory(models.Model):
     car_make = models.CharField(max_length=50)
     car_model = models.CharField(max_length=50)
     def __str__(self):
-        return self.part_name
-
+        return {'part_name': self.part_name}
 
 class Mechanic(models.Model):
     name = models.CharField(max_length=100)
-    services = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+
     def __str__(self):
         return self.name
+
+
+class ServiceOffered(models.Model):
+    service_Offered = models.CharField(max_length=100)
+    def __str__(self):
+        return self.service_Offered
+
+
+class PairMechanic(models.Model):
+    mechanic = models.ForeignKey(Mechanic, on_delete=models.CASCADE)
+    service = models.ForeignKey(ServiceOffered, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.mechanic.name + " offers " + self.service.service_Offered
